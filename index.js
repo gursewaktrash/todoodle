e595c24cf388077c1a690dfd301337152664803d
gursewaktrash <gursewaktrash@gmail.com>
2024-06-09 22:54:24 +0800

    Firebase connected
    Firebase connected

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-const inputField = document.getElementById("input-field")
-const addbutton = document.getElementById("add-button")
+const appSettings = {
+  databaseURL: "https://todoodle-cc086-default-rtdb.firebaseio.com/",
+};
 
-addbutton.addEventListener("click", function() {
-    let inputvalue  = inputField.value
-    //Log the value from input to see if click button works
-    console.log(inputvalue)
-})
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const todoodleListInDB = ref(database, "todoodleList");
+
+const inputField = document.getElementById("input-field");
+const addbutton = document.getElementById("add-button");
+
+addbutton.addEventListener("click", function () {
+  let inputvalue = inputField.value;
+
+  //Function to use firebase 'push' to push inputvalue to database
+  push(todoodleListInDB, inputvalue);
+
+  //Log the value from input to see if click button works
+  console.log(inputvalue);
+});
