63d06e268e6c578f072008e52fa38d3651437f58
gursewaktrash <gursewaktrash@gmail.com>
2024-06-09 18:04:26 +0800

    Basic Button and Click Log
    Basic Button and Click Log

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const inputField = document.getElementById("input-field")
+const addbutton = document.getElementById("add-button")
+
+addbutton.addEventListener("click", function() {
+    let inputvalue  = inputField.value
+    //Log the value from input to see if click button works
+    console.log(inputvalue)
+})
